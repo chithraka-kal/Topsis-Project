3bfa8353c89ec35f0bc286c3d8e253abfdee1867
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-05-19 19:38:11 +0530

    topsis link

--- src/TopsisRatingForm.jsx
+++ src/TopsisRatingForm.jsx
@@ -28,7 +28,7 @@ const TopsisRatingForm = () => {
     const impacts = Array(criteria.length).fill('+');   // All beneficial
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/topsis', {
+      const response = await fetch('https://sachin84.pythonanywhere.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
